1e3e77396174c6a3dd9c7770f89d17b60c07b10d
Abhinay Reddy <xprmnts@protonmail.com>
2017-07-02 23:28:42 -0500

    added functionality to handle multiple cities and places

--- js/app.js
+++ js/app.js
@@ -1,30 +1,10 @@
-//TODO: KnockOut
-var initialPlaces = [
-{
-  name: 'Soho',
-  city: 'London',
-  country: 'United Kingdom',
-  continent: 'Europe',
-  location: { lat: '51.5131699', lng: '-0.144318515' },
-  picture: '',
-  liked: 'false',
-},
-{
-  name: 'Eiffel Tower',
-  city: 'Paris',
-  country: 'France',
-  continent: 'Europe',
-  location: { lat: '48.8583701', lng: '2.2922926' },
-  picture: '',
-  liked: 'false',
-},
-];
-
+// Define Place as an object with properties of a place
 var Place = function (data) {
   this.name = ko.observable(data.name);
   this.city = ko.observable(data.city);
   this.country = ko.observable(data.country);
   this.continent = ko.observable(data.continent);
+  this.tags = ko.observable(data.tags);
   this.location = ko.observableArray([
     data.location,
   ]);
@@ -32,73 +12,121 @@ var Place = function (data) {
   this.liked = ko.observable(data.liked);
 };
 
-var ViewModel = function () {
-  var _this = this;
-  this.placeList = ko.observable([]);
-  this.places = ko.observableArray([]);
-  initialPlaces.forEach(function (placeItem) {
-    _this.placeList().push(new Place(placeItem));
-  });
+// Define City Object
+var City = function (data) {
+  this.city = ko.observable(data.city);
+};
 
+// Define ViewModel
+var ViewModel = function () {
+
+  var _this = this;
+  this.cities = ko.observable([]);
+  this.places = ko.observableArray([]);
+
+  // For each place in list create a list of unique cities to display in filter
+  initialPlaces.forEach(function (placeItem) {
+
+    // If city already exist in list don't push current place as a new city
+    var match = ko.utils.arrayFirst(_this.cities(), function (item) {
+      return placeItem.city === item.city();
+    });
+
+    if (!match) {
+      _this.cities().push(new City(placeItem));
+    }
+
+  }); // End function to create list of unique cities for dropdown filter
+
+  // If a city is clicked in the dropdown render maerkers fo all the associated
+  // places and enable its info window
   this.placesByCity = function (cityName) {
-    _this.places.removeAll();//clear list
+    var markers = [];// Create empty array to hold marker for current city
+    _this.places.removeAll();// Clear places observableArray of history
+
+    var infowindow = new google.maps.InfoWindow();
+
+    // Add each place to the places observableArray that fits the current
+    // city criteria
     initialPlaces.forEach(function (placeItem) {
       if (placeItem.city === cityName.city()) {
+
+        // Push place into places if city criteria matches
         _this.places().push(new Place(placeItem));
+
+        // Create a marker for current place
+        var marker = new google.maps.Marker({
+          position: {
+            lat: parseFloat(placeItem.location.lat),
+            lng: parseFloat(placeItem.location.lng),
+          },
+          map: map,
+          title: placeItem.name,
+        });
+
+        // Add current place marker to markers array
+        markers.push(marker);
+
+        // Add a listener event to each markert that populates info window
+        // on click
+        marker.addListener('click', function () {
+          console.log('attempting to load infowindow');
+          populateInfoWindow(marker, infowindow, placeItem);
+        });
       }
     });
 
+    // set places observableArray to the newly filtered list of places
     _this.places(_this.places());
+
+    // adjust the bounds of the map to fit the newly filtered list of places
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < markers.length; i++) {
+      bounds.extend(markers[i].getPosition());
+    }
+
+    map.fitBounds(bounds);
+
+    // adjust the zoom to a minimum level of 13 to avoid super zoom
+    var zoom = map.getZoom();
+    map.setZoom(zoom > 13 ? 13 : zoom);
   };
 
-  this.mapLocation = function (placeName) {
-    lat = placeName.location()[0].lat;
-    lng = placeName.location()[0].lng;
-    var center = new google.maps.LatLng(lat, lng);
+  // When a place is clicked on the navigation menu center the map on it
+  // and then open up the infowindow related to it
+  this.mapLocation = function (placeItem) {
+    // center map on place coordinates
+    var plat = placeItem.location()[0].lat;
+    var plng = placeItem.location()[0].lng;
+    var center = new google.maps.LatLng(plat, plng);
     map.panTo(center);
-    map.setZoom(14);
-    var marker = new google.maps.Marker({
+
+    // clear infowindow and set marker poisition
+    infowindow = new google.maps.InfoWindow({});
+    marker = new google.maps.Marker({
       position: {
-        lat: parseFloat(placeName.location()[0].lat),
-        lng: parseFloat(placeName.location()[0].lng),
+        lat: parseFloat(plat),
+        lng: parseFloat(plng),
       },
       map: map,
-      title: 'Hi!',
+      title: placeItem.name(),
     });
-    var infowindow = new google.maps.InfoWindow({
-      content: 'Test InfoWindow',
-    });
-    marker.addListener('click', function () {
-      var flickrAPI = 'https://api.flickr.com/services/rest/?';
-      $.getJSON(flickrAPI, {
-        format: 'json',
-        nojsoncallback: 1,
-        method: 'flickr.photos.search',
-        api_key: '853e0dd9b4740b927b87214711cc40f8',
-        tags: 'camden, london, market',
-        per_page: 1,
-        sort: 'relevance',
-      }).done(function (data) {
-        data = data.photos.photo[0];
-        console.log(data);
-        var src = 'https://farm' +
-        data.farm +
-        '.staticflickr.com/' +
-        data.server +
-        '/' +
-        data.id +
-        '_' +
-        data.secret +
-        '.jpg';
-        infowindow.setContent('<img src="' + src + '" alt="' + data.title + '">');
-        infowindow.open(map, marker);
-      }).fail(function (data) {
-        console.log('fail');
-        console.log(data);
-      });
-    });
+
+    // using currently clicked place, an empty infowindow and marker associated
+    // to place populate the infowindow and show it - this is equivalent to
+    // clicking the marker (with the exception of centering the map on coords)
+    populateInfoWindow(marker, infowindow, placeItem);
+
   };
 
 };
+
+function clearOverlays() {
+  for (var i = 0; i < markersArray.length; i++) {
+    markers[i].setMap(null);
+  }
+
+  markersArray.length = 0;
+}
 
 ko.applyBindings(new ViewModel());
--- js/mapapp.js
+++ js/mapapp.js
@@ -1,25 +1,81 @@
 var map;
+var markers = [];
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-          lat: 40.7413549,
-          lng: -73.99802439999996,
-        },
-        zoom: 2,
-      });
-  var tribeca = {
-        lat: 40.719526,
-        lng: -74.0089934,
-      };
-  var marker = new google.maps.Marker({
-        position: tribeca,
-        map: map,
-        title: 'First Marker!',
-      });
-  var infowindow = new google.maps.InfoWindow({
-        content: 'Test InfoWindow',
-      });
-  marker.addListener('click', function () {
-        infowindow.open(map, marker);
-      }); // we used, 40.7413549, -73.99802439999996 or your own!
+  map = new google.maps.Map(document.getElementById('map'), {});
+
+  var infowindow = new google.maps.InfoWindow();
+  var bounds = new google.maps.LatLngBounds();
+
+  // The following group uses the location array to create an array of markers on initialize.
+  for (var i = 0; i < initialPlaces.length; i++) {
+    // Get the position from the location array.
+    var position = initialPlaces[i].location;
+    var title = initialPlaces[i].name;
+
+    // Create a marker per location, and put into markers array.
+    var marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i,
+    });
+
+    // Push the marker to our array of markers.
+    markers.push(marker);
+
+    // Create an onclick event to open an infowindow at each marker.
+    marker.addListener('click', function () {
+      populateInfoWindow(this, infowindow, initialPlaces[this.id]);
+    });
+
+    bounds.extend(markers[i].position);
+  }
+
+  // Extend the boundaries of the map for each marker
+  map.fitBounds(bounds);
+
+  // Use markerclustererplus community repo to cluster markers if appropriate
+  var markerCluster = new MarkerClusterer(map, markers,
+    { imagePath: '/images/m' });
+
+}
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow, placeItem) {
+  infowindow.marker = marker;
+  var flickrAPI = 'https://api.flickr.com/services/rest/?';
+  $.getJSON(flickrAPI, {
+    format: 'json',
+    nojsoncallback: 1,
+    method: 'flickr.photos.search',
+    api_key: '853e0dd9b4740b927b87214711cc40f8',
+    tags: placeItem.tags,
+    per_page: 1,
+    sort: 'relevance',
+  }).done(function (json) {
+    data = json.photos.photo[0];
+    var src = 'https://farm' +
+    data.farm +
+    '.staticflickr.com/' +
+    data.server +
+    '/' +
+    data.id +
+    '_' +
+    data.secret +
+    '.jpg';
+    var infoWindowImage = '<img src="' + src + '" alt="' + data.title + '">';
+    infowindow.setContent(infoWindowImage);
+    infowindow.open(map, marker);
+
+    // infowindow.addListener('closeclick', function () {
+    //   infowindow.setMarker = null;
+    // });
+  }).fail(function () {
+    infowindow.setContent('Sorry! Something went wrong & no image was found');
+    infowindow.open(map, marker);
+  });
+
 }
